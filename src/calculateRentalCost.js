3dfe14915aa1a9cbea8fb7a8276f8bb9e8a122e4
Dmytro Stelmashuk <dimon16295@gmail.com>
2026-03-31 12:18:51 +0300

    task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,24 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  // write code here
+function calculateRentalCost(numberOfDays) {
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const DAY_PRICE = 40;
+
+  if (typeof numberOfDays !== 'number' || numberOfDays < 1) {
+    throw new Error('Invalid days value!');
+  }
+
+  if (numberOfDays >= LONG_TERM) {
+    return numberOfDays * DAY_PRICE - LONG_TERM_DISCOUNT;
+  }
+
+  return numberOfDays >= SHORT_TERM
+    ? numberOfDays * DAY_PRICE - SHORT_TERM_DISCOUNT
+    : numberOfDays * DAY_PRICE;
 }
 
 module.exports = calculateRentalCost;
